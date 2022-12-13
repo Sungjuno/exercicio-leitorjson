f9dbadd840163b15ff38ab1cd485b1ab22f3c954
sung <nosungju@gmail.com>
2022-12-13 15:04:05 -0300

    leitor json

--- exercicioCsharp00/Program.cs
+++ exercicioCsharp00/Program.cs
@@ -1,7 +1,38 @@
-﻿var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+﻿using System.Text.Json;
 
-app.MapGet("/", () => "Hello World!");
+namespace LeitorJson;
 
-app.Run();
+    class Program
 
+    {
+
+        public static void Main(string[] args)
+
+        {
+        var quantidadeArquivo = Directory.GetFiles("/Users/sung/Projects/exercicioCsharp00/exercicioCsharp00/imports");
+        var listaClientes = new List<Cliente>() { };
+
+        for ( int i = 1; i <= quantidadeArquivo.Length; i++)
+        {
+            var text = File.ReadAllText($"/Users/sung/Projects/exercicioCsharp00/exercicioCsharp00/imports/clientes{i}.json");
+            var jsonClienteObjeto = JsonSerializer.Deserialize<Cliente>(text, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            if(jsonClienteObjeto != null)
+            {
+                listaClientes.Add(jsonClienteObjeto);
+            }
+            Console.WriteLine(listaClientes[i-1].Nome + "  ----  " + listaClientes[i-1].Telefone);
+        }
+
+        foreach( var arquivo in quantidadeArquivo) { }
+
+    }
+
+
+    }
+
+    class Cliente
+
+    {
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+    }
